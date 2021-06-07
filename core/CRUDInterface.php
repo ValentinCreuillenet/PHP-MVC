254a6a97f518dceff6033a5a56d16fe32898574f
Valentin <valentincreuillenet@hotmal.fr>
2021-06-07 14:36:14 +0000

    commented code

--- core/CRUDInterface.php
+++ core/CRUDInterface.php
@@ -2,10 +2,33 @@
 
 interface CRUDInterface
 {
-    public function create($args): self;
-    public function retrieve($id): self;
-    public function update($id): bool;
-    public function delete($id): bool;
+    /**
+     * Crée une nouvelle entité dan la bdd 
+     * @param array Tableau assosiatif des données a insérer
+     * @return object L'objet correspondant
+     */
+    public function create(array $args): object;
+
+    /**
+     * Récupère une entité par son ID
+     * @param int L'id de l'entité a récupéré
+     * @return object L'entité récupérée
+     */
+    public function retrieve(int $id) : object;
+
+    /**
+     * Met a jour une entité dans la BDD
+     * @param object Le nouvelle état de l'entité
+     * @return bool true si ça c'est bien passer et faux sinon
+     */
+    public function update(object $entity): bool;
+
+    /**
+     * Supprime une entité de la bdd
+     * @param int L'id de l'entité a supprimer
+     * @return bool true si ça c'est bien passer et fauc sinon
+     */
+    public function delete(int $id): bool;
 }
 
 ?>
--- core/RepositoryInterface.php
+++ core/RepositoryInterface.php
@@ -2,9 +2,18 @@
 
 interface RepositoryInterface
 {
-    public function getAll($collection): array;
-    public function getAllBy($collection, $id): array;
+    /**
+     * Récupère tout les éléments d'une table/collection
+     * @return array les éléments de la table/collection
+     */
+    public function getAll(): array;
 
+    /**
+     * Reécupère tout les éléments d'une table/collection qui correspondent a des param
+     * @param array Les arguments qui servent a vérifier les éléments a récuperer
+     * @return array Les éléments remontées par la requête
+     */
+    public function getAllBy(array $args): array;
 }
 
 ?>
