70c16657631c30a784e3869406a092db3b2a9b24
Valentin Creuillenet <valentincreuillenet@hotmail.fr>
2021-06-08 12:54:45 +0000

    prise en charge du fichier de routage

--- core/Routing.php
+++ core/Routing.php
@@ -1,4 +1,16 @@
 <?php
 
 class Routing{
+
+    private $config;
+
+    public function __construct(){
+
+        $this->config = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/routing.json'),true); 
+
+    }
+
+    public function test(){
+        return $this->config;
+    }
 }
