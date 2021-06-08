e44ff04c2d5d7d5e5524f25bb4feef8c63302204
Valentin Creuillenet <valentincreuillenet@hotmail.fr>
2021-06-08 09:44:22 +0000

    prise en charge du fichier config/database.json

--- core/DAO.php
+++ core/DAO.php
@@ -1,11 +1,45 @@
 <?php
 
-class DAO implements CRUDInterface, RepositoryInterface{
+abstract class DAO implements CRUDInterface, RepositoryInterface{
 
     protected $pdo;
 
     public function __construct(){
-        $this->$pdo = new PDO("dsn","username","password");
+        $database = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'].'/config/database.json'),true);
+        $dsn = "{$database['driver']}:dbname={$database['dbname']};host={$database['host']};port={$database['port']}";
+        $username = $database['username'];
+        $password = $database['password'];
+        $this->pdo = new PDO($dsn,$username,$password);
+    }
+
+    public function create(array $args): object
+    {
+        return null;
+    }
+
+    public function retrieve(int $id) : object
+    {
+        return null;
+    }
+
+    public function update(object $entity): bool
+    {
+        return null;
+    }
+
+    public function delete(int $id): bool
+    {
+        return null;
+    }
+
+    public function getAll(): array
+    {
+        return null;
+    }
+
+    public function getAllBy(array $args): array
+    {
+        return null;
     }
 }
 
